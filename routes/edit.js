7bbd2685bf160eb8a4d403bee94f9154663e3bbd
kjerrya <lifthz@gmail.com>
2018-01-12 09:36:15 -0500

    Discovered server issue may be due to route conflicts...

--- routes/edit.js
+++ routes/edit.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const editRoute = express.Router();
 const mainController = require('../controller/main');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-editRoute.get('/:id', mainController.each);
+//editRoute.get('/:id', mainController.each);
 editRoute.get('/edit', mainController.edit);
 editRoute.get('/editAll', mainController.editAll);
-editRoute.get('/edit/add', mainController.edit);
-editRoute.put('/edit/:id', mainController.update);
-editRoute.post('edit/add', mainController.add);
+//editRoute.get('/edit/add', mainController.edit);
+//editRoute.put('/edit/:id', mainController.update);
+//editRoute.post('edit/add', mainController.add);
 editRoute.delete('/edit', mainController.delete);
 
 
